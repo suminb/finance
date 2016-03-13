f03c8811d5b7ab37639783794a734bffbed02e77
Sumin Byeon <suminb@gmail.com>
2016-03-14 02:18:48 +0900

    Add test cases for dealing with records

--- tests/test_models.py
+++ tests/test_models.py
@@ -12,8 +12,34 @@ def test_transaction(app):
             t.state = 'xxxx'
 
 
-def test_records(app, account_checking):
-    pass
+def test_records(app, account_checking, asset_krw):
+    with app.app_context():
+        with Transaction.create() as t:
+            record = Record.create(
+                created_at=make_date('2016-03-14'), transaction=t,
+                account=account_checking, asset=asset_krw,
+                quantity=1000)
+
+            # Make sure the record type has been set implictly
+            assert 'deposit' == record.type
+
+        with Transaction.create() as t:
+            record = Record.create(
+                created_at=make_date('2016-03-14'), transaction=t,
+                account=account_checking, asset=asset_krw,
+                quantity=-2000)
+
+            # Make sure the record type has been set implictly
+            assert 'withdraw' == record.type
+
+        with Transaction.create() as t:
+            record = Record.create(
+                created_at=make_date('2016-03-14'), transaction=t,
+                account=account_checking, asset=asset_krw,
+                quantity=3000, type='balance_adjustment')
+
+            # Make sure the record type has been set explicitly
+            assert 'balance_adjustment' == record.type
 
 
 def test_net_worth(app, account_checking, account_sp500, asset_krw,
