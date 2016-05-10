6bca1b9925d22e0171f9749155ec3d006c077e87
Sumin Byeon <suminb@gmail.com>
2016-05-10 12:50:16 +0000

    Login to 8percent

--- finance/providers/_8percent.py
+++ finance/providers/_8percent.py
@@ -1,28 +1,40 @@
-from bs4 import BeautifulSoup
 import requests
 
 from finance.providers import Provider
 
 
-LOGIN_URL = 'https://8percent.kr/user/login'
+LOGIN_URL = 'https://8percent.kr/user/login/'
 
 
 class _8Percent(Provider):
     def login(self, username, password):
         """Returns a cookie string if login is successful."""
-        csrf_token = self.get_csrf_token(LOGIN_URL)
+        client = requests.session()
 
+        client.get(LOGIN_URL)
+        csrf_token = client.cookies['csrftoken']
+
+        headers = {
+            'Origin': 'https://8percent.kr',
+            'Referer': LOGIN_URL,
+        }
         data = {
             'csrfmiddlewaretoken': csrf_token,
+            'type': 'login',
             'email': username,
             'password': password,
         }
-        resp = requests.post(LOGIN_URL, data=data)
-        import pdb; pdb.set_trace()
-        pass
+        cookies = client.cookies
+        cookies = {k: v for k, v in zip(cookies.keys(), cookies.values())}
+        resp = client.post(LOGIN_URL, headers=headers, data=data,
+                           cookies=cookies)
+        return resp
+
+    def get_cookies(self, url):
+        resp = requests.get(url)
+        cookies = resp.cookies
+        return {k: v for k, v in zip(cookies.keys(), cookies.values())}
 
     def get_csrf_token(self, url):
         resp = requests.get(url)
-        soup = BeautifulSoup(resp.text, 'html.parser')
-        csrf_input = soup.find('input', {'name': 'csrfmiddlewaretoken'})
-        return csrf_input['value']
+        return resp.cookies['csrftoken']
