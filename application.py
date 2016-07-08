67106009b4a64d45de514a96f6db0659b0d118c2
Sumin Byeon <suminb@gmail.com>
2016-07-09 00:55:50 +0900

    Ready to deploy on Heroku

--- application.py
+++ application.py
@@ -2,10 +2,9 @@ import os
 
 from finance import create_app
 
-application = create_app()
-
 
 if __name__ == '__main__':
+    application = create_app()
     host = os.environ.get('HOST', '0.0.0.0')
     port = int(os.environ.get('PORT', 8002))
     debug = bool(os.environ.get('DEBUG', False))
