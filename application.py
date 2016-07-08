5f33b5acea96c6fd4766be83ca229133be6f95c1
Sumin Byeon <suminb@gmail.com>
2016-07-09 00:40:30 +0900

    Debug flag

--- application.py
+++ application.py
@@ -8,5 +8,6 @@ application = create_app()
 if __name__ == '__main__':
     host = os.environ.get('HOST', '0.0.0.0')
     port = int(os.environ.get('PORT', 8002))
+    debug = bool(os.environ.get('DEBUG', False))
 
-    application.run(host=host, port=port)
+    application.run(host=host, port=port, debug=debug)
