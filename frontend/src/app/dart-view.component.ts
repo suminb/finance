150942fac00ec7812201a24f7fb3a36e56656f82
Sumin Byeon <suminb@gmail.com>
2017-05-31 00:27:22 +0900

    Display raw HTML

--- frontend/src/app/dart-view.component.ts
+++ frontend/src/app/dart-view.component.ts
@@ -6,7 +6,6 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 
 import { DartService } from './dart.service';
-import { DartReport } from './dart.model';
 
 @Component({
     selector: 'app-root',
