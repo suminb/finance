5d048ae4ba7b12a45101a0dbc1e5207ae3360875
Sumin Byeon <suminb@gmail.com>
2017-11-07 00:50:24 +0900

    Use of a null resource

--- infra/main.tf
+++ infra/main.tf
@@ -22,11 +22,29 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
+variable "lambda_filename" {
+  default = "fetch_asset_values.zip"
+}
+
+# NOTE: I would like to execute the following commands before creating the
+# `fetch_asset_values_lambda` resource, but I haven't figured out how to do so.
+resource "null_resource" "build_lambda" {
+  provisioner "local-exec" {
+    command = <<EOF
+      pip install -r ../requirements.txt -t build
+      cp main.py build/
+      zip -r fetch_asset_values.zip build
+    EOF
+  }
+}
+
 resource "aws_lambda_function" "fetch_asset_values_lambda" {
-  filename         = "fetch_asset_values.zip"
+  filename         = "${var.lambda_filename}"
   function_name    = "fetch_asset_values"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "main.handler"
-  source_code_hash = "${base64sha256(file("fetch_asset_values.zip"))}"
+  source_code_hash = "${base64sha256(file("${var.lambda_filename}"))}"
   runtime          = "python3.6"
+
+  depends_on = ["null_resource.build_lambda"]
 }
