176e5cf44ed92c0dc75ba1667d30d1db594da5c0
Sumin Byeon <suminb@gmail.com>
2018-01-10 01:58:12 +0900

    Define a common interface for providers

--- finance/providers/provider.py
+++ finance/providers/provider.py
@@ -1,10 +1,29 @@
+from finance.models import Granularity
+
+
 class Provider(object):
     pass
 
 
 class AssetValueProvider(Provider):
-    pass
+    def __init__(self):
+        raise NotImplementedError
+
+    def asset_values(self, evaulated_at, granularity=Granularity.day):
+        """Returns a list of asset value records (list of tuples).
+
+        (datetime, open, high, low, close, volume)
+        """
+        raise NotImplementedError
 
 
 class RecordProvider(Provider):
-    pass
+    def __init__(self):
+        raise NotImplementedError
+
+    def records(self):
+        """Returns list of financial transactions (list of tuples).
+
+        (TODO: Format to be determined)
+        """
+        raise NotImplementedError
