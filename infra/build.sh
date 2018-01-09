e6ef351aaf92d1602661284b92d17a6dad5a16cf
Sumin Byeon <suminb@gmail.com>
2018-01-09 14:02:23 +0000

    Build with pre-built Python packages

--- infra/build.sh
+++ infra/build.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+set -e
+
 # NOTE: Ideally, we would like to perform the following task in Terraform
 
-pip install -r ../requirements.txt -t build
-cp main.py build/
+S3_BUCKET="suminb-test"
+ARTIFACT="fetch_asset_values.zip"
+
+# pip install -r ../requirements.txt -t build
+# NOTE: Instead of directly building Python requirements, we pre-built those
+# packages on an Amazon Linux instance and made an archive.
+aws s3 cp s3://$S3_BUCKET/build.tgz .
+rm -rf build
+tar zxf build.tgz
+
+cp lambda.py build/
 cp -r ../finance build/
-cd build
+pushd build
 find . -name "*.pyc" -delete
-zip -r ../fetch_asset_values.zip .
+find . -name __pycache__ -delete
+rm -rf $(find . -name tests)
+rm ../$ARTIFACT
+zip -r ../$ARTIFACT .
+popd
+aws s3 cp fetch_asset_values.zip s3://$S3_BUCKET/
