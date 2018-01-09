df20d9e667243825a9cee66a360121f99a6f3489
Sumin Byeon <suminb@gmail.com>
2018-01-10 02:13:24 +0900

    Import AssetValueProvier and RecordProvider

--- finance/providers/__init__.py
+++ finance/providers/__init__.py
@@ -2,7 +2,9 @@ from finance.providers._8percent import _8Percent
 from finance.providers.dart import Dart
 from finance.providers.kofia import Kofia
 from finance.providers.miraeasset import Miraeasset
-from finance.providers.provider import Provider
+from finance.providers.provider import AssetValueProvider, Provider, \
+    RecordProvider
 
 
-__all__ = ['_8Percent', 'Dart', 'Kofia', 'Miraeasset', 'Provider']
+__all__ = ['_8Percent', 'AssetValueProvider', 'Dart', 'Kofia', 'Miraeasset',
+           'Provider', 'RecordProvider']
