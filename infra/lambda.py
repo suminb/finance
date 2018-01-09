0dcfb8c49fe0c02cfc2c5987ad5e93ad8da7435f
Sumin Byeon <suminb@gmail.com>
2018-01-09 13:52:01 +0000

    Import asset values for multiple entities

--- infra/lambda.py
+++ infra/lambda.py
@@ -1,4 +1,5 @@
 import math
+import os
 
 from logbook import Logger
 from pandas_datareader import data
@@ -16,42 +17,47 @@ log = Logger('finance')
 # TODO: Write logs to CloudWatch
 
 def handler(event, context):
-    code = event['code']
+    codes = event['codes']
     config = {
         'SQLALCHEMY_DATABASE_URI': event['db_url'],
     }
 
     app = create_app(__name__, config=config)
     with app.app_context():
+        for code in codes:
+            fetch_asset_values(code)
+
+
+def fetch_asset_values(code):
+    try:
+        asset = Asset.get_by_symbol(code)
+    except AssetNotFoundException:
+        log.info('Asset {0} does not exist. Creating an Asset record...',
+                 code)
+        asset = Asset.create(name=code, code=code, type=AssetType.stock)
+
+    df = data.DataReader(code, 'yahoo', parse_date(-30), parse_date(-1))
+    for record in df.to_records():
+        date, open_, high, low, close_, adj_close, volume = record
+
+        if any([math.isnan(x) for x in
+                [open_, high, low, close_, adj_close, volume]]):
+            log.warn('Some value is NaN. Skipping this record.')
+            continue
+
         try:
-            asset = Asset.get_by_symbol(code)
-        except AssetNotFoundException:
-            log.info('Asset {0} does not exist. Creating an Asset record...',
-                     code)
-            asset = Asset.create(name=code, code=code, type=AssetType.stock)
-
-        df = data.DataReader(code, 'yahoo', parse_date(-30), parse_date(-1))
-        for record in df.to_records():
-            date, open_, high, low, close_, adj_close, volume = record
-
-            if any([math.isnan(x) for x in
-                    [open_, high, low, close_, adj_close, volume]]):
-                log.warn('Some value is NaN. Skipping this record.')
-                continue
-
-            try:
-                # NOTE: Without casting `volume` into an integer type, it will
-                # produce an error of
-                # `psycopg2.ProgrammingError: can't adapt type 'numpy.int64'`
-                # Need to figure out why.
-                asset_value = AssetValue.create(
-                    evaluated_at=date, granularity=Granularity.day,
-                    asset=asset, open=open_, high=high, low=low, close=close_,
-                    volume=int(volume))
-                log.info('Record has been create: {0}', asset_value)
-            except IntegrityError:
-                log.warn('AssetValue for {0} on {1} already exist', code, date)
-                db.session.rollback()
+            # NOTE: Without casting `volume` into an integer type, it will
+            # produce an error of
+            # `psycopg2.ProgrammingError: can't adapt type 'numpy.int64'`
+            # Need to figure out why.
+            asset_value = AssetValue.create(
+                evaluated_at=date, granularity=Granularity.day,
+                asset=asset, open=open_, high=high, low=low, close=close_,
+                volume=int(volume))
+            log.info('Record has been create: {0}', asset_value)
+        except IntegrityError:
+            log.warn('AssetValue for {0} on {1} already exist', code, date)
+            db.session.rollback()
 
     log.info('Asset values for {0} have been imported', code)
 
@@ -60,5 +66,8 @@ def handler(event, context):
 
 
 if __name__ == '__main__':
-    event = {'code': 'ESRT'}
+    event = {
+        'db_url': os.environ['DB_URL'],
+        'codes': ['ESRT', 'NVDA']
+    }
     handler(event, None)
