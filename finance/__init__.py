599e8caa0908b9cc7d8e5a991615ce67f848d2f5
Sumin Byeon <suminb@gmail.com>
2018-08-31 00:05:46 +0900

    Update version info (0.3.3)

--- finance/__init__.py
+++ finance/__init__.py
@@ -9,7 +9,7 @@ from flask_login import current_user, LoginManager
 from logbook import Logger, StreamHandler
 
 
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 __author__ = 'Sumin Byeon'
 __email__ = 'suminb@gmail.com'
 
