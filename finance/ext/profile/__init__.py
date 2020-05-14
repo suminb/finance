37aa826585d07ecc25ad75fe98acbd1b6900afd8
Sumin Byeon <suminb@gmail.com>
2020-05-15 02:00:19 +0900

    Documentation

--- finance/ext/profile/__init__.py
+++ finance/ext/profile/__init__.py
@@ -5,6 +5,22 @@ from finance.ext.profile.naver_finance import fetch_naver_profile
 
 
 def fetch_profile(provider: str, symbol: str):
+    """Fetches a company profile.
+
+    Usage:
+
+        >>> profile = fetch_profile("naver", "063170")
+        >>> profile.name
+        서울옥션
+        >>> profile.current_price
+        4300
+        >>> profile.outstanding_shares
+        16917500
+        >>> profile.eps
+        -395
+        >>> profile.bps
+        4344
+    """
     if provider == "naver":
         return fetch_naver_profile(symbol)
     else:
