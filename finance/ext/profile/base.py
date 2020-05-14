a5670c97abe4cf45c0cca425714b361eea060862
Sumin Byeon <suminb@gmail.com>
2020-05-15 01:30:17 +0900

    Adjust default values

--- finance/ext/profile/base.py
+++ finance/ext/profile/base.py
@@ -1,10 +1,12 @@
+from math import nan
+
+
 class BaseProfile:
     def __init__(self, symbol: str):
         self.symbol = symbol
-        self.name = ""
-        self.current_price = 0
-        self.outstanding_shares = 0
-        raise NotImplementedError
+        self.name = None
+        self.current_price = nan
+        self.outstanding_shares = nan
 
     def parse(self, raw: str):
         raise NotImplementedError
