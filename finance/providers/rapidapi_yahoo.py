063ed5b89edf3f37ba6fcfff8a9fa20694970dcc
Sumin Byeon <suminb@gmail.com>
2020-12-12 22:09:03 +0900

    Introduce Financials object

--- finance/providers/rapidapi_yahoo.py
+++ finance/providers/rapidapi_yahoo.py
@@ -12,45 +12,51 @@ headers = {
 }
 
 
+# TODO: Move this elsewhere
+class Financials:
+    def __init__(self, data: dict):
+        self.data = data
+
+    @property
+    def market_cap(self):
+        return self.data["price"]["marketCap"]["raw"]
+
+    @property
+    def most_recent_yearly_earnings(self):
+        earnings = self.data["earnings"]["financialsChart"]["yearly"]
+
+        # As a matter of fact, `date` is actually year
+        recent_year = max(x["date"] for x in earnings)
+
+        # It seems like the annual earnings are in chronological order,
+        # but there is no such guarantee and thus we added an assertion statement here.
+        recent_earnings = earnings[-1]
+        assert recent_earnings["date"] == recent_year
+
+        return recent_earnings
+
+    @property
+    def most_recent_quarterly_earnings(self):
+        earnings = self.data["earnings"]["financialsChart"]["quarterly"]
+
+        # 'quarter' looks like "1Q2020", "4Q2019", etc.
+        recent_quarter = max(x["date"].split("Q")[::-1] for x in earnings)
+        recent_quarter = "Q".join(recent_quarter[::-1])
+
+        # It seems like the quarterly earnings are in chronological order,
+        # but there is no such guarantee and thus we added an assertion statement here.
+        recent_earnings = earnings[-1]
+        assert recent_earnings["date"] == recent_quarter
+
+        return recent_earnings
+
+
 def get_financials(symbol: str, region="US"):
     url = f"https://{API_HOST}/stock/v2/get-financials"
     params = {"symbol": symbol, "region": region}
     resp = requests.get(url, headers=headers, params=params)
 
-    return json.loads(resp.text)
-
-
-def get_market_cap(financials: dict):
-    return financials["price"]["marketCap"]["raw"]
-
-
-def get_most_recent_yearly_earnings(financials: dict):
-    earnings = financials["earnings"]["financialsChart"]["yearly"]
-
-    # As a matter of fact, `date` is actually year
-    recent_year = max(x["date"] for x in earnings)
-
-    # It seems like the annual earnings are in chronological order,
-    # but there is no such guarantee and thus we added an assertion statement here.
-    recent_earnings = earnings[-1]
-    assert recent_earnings["date"] == recent_year
-
-    return recent_earnings
-
-
-def get_most_recent_quarterly_earnings(financials: dict):
-    earnings = financials["earnings"]["financialsChart"]["quarterly"]
-
-    # 'quarter' looks like "1Q2020", "4Q2019", etc.
-    recent_quarter = max(x["date"].split("Q")[::-1] for x in earnings)
-    recent_quarter = "Q".join(recent_quarter[::-1])
-
-    # It seems like the quarterly earnings are in chronological order,
-    # but there is no such guarantee and thus we added an assertion statement here.
-    recent_earnings = earnings[-1]
-    assert recent_earnings["date"] == recent_quarter
-
-    return recent_earnings
+    return Financials(json.loads(resp.text))
 
 
 def get_historical_data(symbol: str, region="US"):
