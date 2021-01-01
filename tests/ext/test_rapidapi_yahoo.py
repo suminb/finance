c6ce4a2d0bb48f3fdbf82ad2214bd7edb776f668
Sumin Byeon <suminb@gmail.com>
2021-01-01 22:59:56 +0900

    Test cached data

--- tests/ext/test_rapidapi_yahoo.py
+++ tests/ext/test_rapidapi_yahoo.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from finance.ext.rapidapi.yahoo import cache_exists
+from finance.ext.rapidapi.yahoo import cache_exists, load_cache
 import json
 import os
 
@@ -7,19 +7,15 @@ from finance.ext.rapidapi.yahoo.models import Financials, Profile, HistoricalDat
 
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
-
-
-def load_test_data(filename):
-    with open(f"tests/ext/rapidapi/yahoo/{filename}") as fin:
-        return json.loads(fin.read())
+CACHE_DIR = BASE_PATH + "/rapidapi/yahoo"
 
 
 def test_cache():
-    assert cache_exists("profile", "NET", "US", BASE_PATH + "/rapidapi/yahoo")
+    assert cache_exists("profile", "NET", "US", CACHE_DIR)
 
 
 def test_financials():
-    financials = Financials(load_test_data("financials-NET.json"))
+    financials = Financials(load_cache("financials", "NET", "US", CACHE_DIR))
     assert financials.market_cap == 25324675072
 
     assert financials.most_recent_yearly_earnings["date"] == 2019
@@ -32,11 +28,11 @@ def test_financials():
 
 
 def test_profile():
-    profile = Profile(load_test_data("profile-TSLA.json"))
+    profile = Profile(load_cache("profile", "TSLA", "US", CACHE_DIR))
     assert profile.sector == "Consumer Cyclical"
 
 
 def test_historical_data():
-    historical_data = HistoricalData(load_test_data("historical_data-MSFT.json"))
+    historical_data = HistoricalData(load_cache("historical_data", "MSFT", "US", CACHE_DIR))
     assert historical_data.first_trade_date == datetime(1986, 3, 13, 14, 30)
     assert historical_data.most_recent_price == 213.26
