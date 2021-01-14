2beb098266b2f2812dc8623926708c35b47d932f
Sumin Byeon <suminb@gmail.com>
2021-01-15 02:45:42 +0900

    Alembic script to add financial table

---
+++ alembic/versions/8b831ca52f15_add_financial_table.py
@@ -0,0 +1,40 @@
+"""Add financial table
+
+Revision ID: 8b831ca52f15
+Revises: 3127ef2df8ce
+Create Date: 2021-01-15 02:17:04.860188
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8b831ca52f15'
+down_revision = '3127ef2df8ce'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "financial",
+        sa.Column("id", sa.BigInteger(), autoincrement=False, nullable=False),
+        sa.Column("item", sa.String(), nullable=False),
+        sa.Column(
+            "granularity",
+            # NOTE: 'quarterly' can be both an adjective and an adverb.
+            sa.Enum("quarterly", "annual", name="financial_granularity"),
+            nullable=False,
+        ),
+        sa.Column("year", sa.Integer(), nullable=False),
+        sa.Column("quarter", sa.SmallInteger(), nullable=False),
+        sa.Column("value", sa.Numeric(precision=20, scale=4), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("item", "granularity", "year", "quarter"),
+    )
+
+
+def downgrade():
+    op.drop_table("financial")
+    op.execute("DROP TYPE financial_granularity")
