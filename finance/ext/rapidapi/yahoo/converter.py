7b39995a5df0e1709634c3244c9397b506cbbde3
Sumin Byeon <suminb@gmail.com>
2021-01-15 03:15:28 +0900

    Converter for financials

--- finance/ext/rapidapi/yahoo/converter.py
+++ finance/ext/rapidapi/yahoo/converter.py
@@ -1,4 +1,5 @@
-from finance.models import AssetValue, Granularity
+from finance.models import AssetValue, Financial, Granularity
+from finance.ext.rapidapi.yahoo.models import Financials
 
 
 def convert_prices_to_asset_values(asset_id, base_asset_id, prices):
@@ -17,3 +18,31 @@ def convert_prices_to_asset_values(asset_id, base_asset_id, prices):
             volume=price["volume"],
             ignore_if_exists=True,
         )
+
+
+def convert_financials(asset_id, financials: Financials):
+    keys = ["revenue", "earnings"]
+    for earning in financials.quarterly_earnings:
+        quarter, year = earning["date"].split("Q")
+        for key in keys:
+            yield Financial.create(
+                asset_id=asset_id,
+                key=key,
+                granularity="quarterly",
+                year=int(year),
+                quarter=int(quarter),
+                value=earning[key],
+                ignore_if_exists=True,
+            )
+
+    for earning in financials.yearly_earnings:
+        for key in keys:
+            yield Financial.create(
+                asset_id=asset_id,
+                key=key,
+                granularity="annual",
+                year=int(earning["date"]),
+                quarter=0,
+                value=earning[key],
+                ignore_if_exists=True,
+            )
