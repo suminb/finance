293536f9a20e5ca87d9d82d3afd44de1857ddacb
Sumin Byeon <suminb@gmail.com>
2021-05-25 01:32:36 +0900

    Downgrade is not supported

--- alembic/versions/ecaa0e473819_alter_granularity_and_asset_value.py
+++ alembic/versions/ecaa0e473819_alter_granularity_and_asset_value.py
@@ -30,7 +30,28 @@ def upgrade():
     op.execute("ALTER TABLE asset_value ALTER COLUMN high TYPE numeric(18, 10)")
     op.execute("ALTER TABLE asset_value ALTER COLUMN volume TYPE numeric(18, 10)")
 
+    op.execute("ALTER TYPE asset_type RENAME VALUE 'currency' TO 'fiat_currency'")
+    op.execute("ALTER TYPE asset_type ADD VALUE 'crypto_currency'")
+
 
 def downgrade():
-    # NOTE: There is no way back...
-    op.execute("ALTER TYPE ticker_granularity RENAME TO granularity")
+    """Fully functional downgrade is not supported. Shall be used in testing environments only."""
+    raise RuntimeError("Downgrade is not supported")
+    # op.execute("ALTER TABLE asset_value ALTER COLUMN granularity TYPE VARCHAR(255)")
+    # op.execute("DROP TYPE ticker_granularity")
+    # op.execute("CREATE TYPE granularity AS ENUM('1sec', '1min', '5min', '1hour', '1day', '1week', '1month', '1year')")
+    # # NOTE: This will introduce errors if there is non-mappable values in asset_value.granularity.
+    # op.execute("ALTER TABLE asset ALTER COLUMN granularity TYPE granularity USING granularity::granularity")
+
+    # op.execute("ALTER TABLE asset_value ALTER COLUMN source TYPE VARCHAR(255)")
+    # op.execute("DROP TYPE asset_value_source")
+    # op.execute("CREATE TYPE asset_value_source AS ENUM('yahoo', 'google', 'kofia')")
+    # # NOTE: This will introduce errors if there is non-mappable values in asset_value.source.
+    # op.execute("ALTER TABLE asset ALTER COLUMN source TYPE source USING source::asset_value_source")
+    #
+    # op.execute("ALTER TABLE asset ALTER COLUMN type TYPE VARCHAR(255)")
+    # op.execute("DROP TYPE asset_type")
+    # op.execute("UPDATE asset SET type='currency' WHERE type='fiat_currency' OR type='crypto_currency'")
+    # op.execute(
+    #     "CREATE TYPE asset_type AS ENUM('currency', 'stock', 'bond', 'p2p_bond', 'security', 'fund', 'commodity')")
+    # op.execute("ALTER TABLE asset ALTER COLUMN type TYPE asset_type USING type::asset_type")
