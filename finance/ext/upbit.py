5549c6646ca63c551e52fc68e0c7b81721e13ee7
Sumin Byeon <suminb@gmail.com>
2021-05-26 02:56:49 +0900

    Continuous fetching

--- finance/ext/upbit.py
+++ finance/ext/upbit.py
@@ -4,6 +4,7 @@
 
 from datetime import datetime
 import json
+import time
 
 import requests
 from sqlalchemy.exc import IntegrityError
@@ -60,24 +61,50 @@ def insert_supported_currencies():
 #   ...
 # ]
 #
-def fetch_tickers(currency, base_currency="KRW", minutes=15):
+def fetch_tickers(currency, base_currency="KRW", minutes=15, until=datetime.utcnow()):
     url = f"https://api.upbit.com/v1/candles/minutes/{minutes}"
     params = {
         "market": f"{base_currency}-{currency}",
         "count": 200,
+        "to": until.strftime("%Y-%m-%d %H:%M:%H"),
     }
     resp = requests.get(url, params=params)
     data = json.loads(resp.text)
     return data
 
 
+def fetch_tickers_continuously(currency, base_currency="KRW", minutes=15, until=datetime.utcnow()):
+    while True:
+        result = fetch_tickers(currency, base_currency, minutes, until)
+        if result:
+            for r in result:
+                yield r
+        else:
+            break
+        last_datetime_str = result[-1]["candle_date_time_utc"]
+        until = datetime.strptime(last_datetime_str, "%Y-%m-%dT%H:%M:%S")
+        time.sleep(0.1)
+
+
+# Perhaps we should move this elsewhere
+granularity_to_minutes = {
+    Granularity.min: 1,
+    Granularity.three_min: 3,
+    Granularity.fifteen_min: 15,
+    Granularity.hour: 60,
+    Granularity.four_hour: 240,
+}
+
+
 def insert_tickers(
         currency: str,
-        granularity: str = Granularity.fifteen_min
+        base_currency="KRW",
+        granularity: str = Granularity.fifteen_min,
+        until=datetime.utcnow(),
 ):
-    base_asset = Asset.get_by_symbol("KRW")
+    base_asset = Asset.get_by_symbol(base_currency)
     asset = Asset.get_by_symbol(currency)
-    records = fetch_tickers(currency)
+    records = fetch_tickers_continuously(currency, base_currency, granularity_to_minutes[granularity], until)
     for r in records:
         evaluated_at = datetime.strptime(r["candle_date_time_utc"], "%Y-%m-%dT%H:%M:%S")
         try:
