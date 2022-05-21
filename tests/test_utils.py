cc7f68556adc67fbb4c887d340dec2360f73305f
Sumin Byeon <suminb@gmail.com>
2022-05-21 15:09:13 +0900

    Parse dollar values

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -14,6 +14,7 @@ from finance.utils import (
     parse_date,
     parse_datetime,
     parse_decimal,
+    parse_dollar_value,
     parse_int,
     parse_stock_code,
     parse_stock_records,
@@ -90,6 +91,23 @@ def test_dict_reader():
     d = DictReader({"key": "value"})
     assert d["key"] == "value"
     assert d.key == "value"
+
+
+@pytest.mark.parametrize("pair", [
+    ("$0", 0),
+    ("$.75", 0.75),
+    ("$1.00", 1.0),
+    ("$423", 423),
+    ("$29.99", 29.99),
+    ("$1,000.00", 1000.0),
+    ("$12,000,000", 12000000),
+    ("0", 0),
+    ("1,000,000,000.99", 1000000000.99),
+])
+def test_dollar_value(pair):
+    value, expected = pair
+    actual = parse_dollar_value(value)
+    assert expected == actual
 
 
 def test_extract_numbers():
