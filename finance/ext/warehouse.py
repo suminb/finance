ce8fba0fa7601c3ab51c3e2e6dc110c0823f85e3
Sumin Byeon <suminb@gmail.com>
2023-10-25 22:55:52 +0900

    feat: Leave skip markers

--- finance/ext/warehouse.py
+++ finance/ext/warehouse.py
@@ -111,7 +111,7 @@ def fetch_historical_data(
 def refresh_tickers_and_historical_data(
     region: str,
     tickers_source: pd.DataFrame,
-    base_path: str,
+    staging_path: str,
     tickers_target_path: str,
     historical_target_path: str,
     delay_factor: float = 2.0,
@@ -160,7 +160,9 @@ def refresh_tickers_and_historical_data(
         "updated_at",
     ]
 
-    os.makedirs(base_path, exist_ok=True)
+    # profile_base_path = os.path.join(staging_path, "profiles")
+    historical_base_path = os.path.join(staging_path, "historical")
+    os.makedirs(historical_base_path, exist_ok=True)
 
     with Progress() as progress:
         task = progress.add_task("[red]Fetching", total=len(symbols))
@@ -168,16 +170,27 @@ def refresh_tickers_and_historical_data(
             progress.update(task, description=f"Fetching[{symbol}]", advance=1)
             dt = datetime.utcnow().strftime("%Y%m%d")
 
+            skip_marker_path = os.path.join(
+                historical_base_path, f"{region}-{symbol}-{dt}.skip"
+            )
+            if os.path.exists(skip_marker_path):
+                log.info(f"Skipping {symbol}...")
+                continue
+
             try:
                 profile, history_new = fetch_profile_and_historical_data(symbol, region)
             except Exception as e:
                 log.warn(f"{symbol}: {e}")
+                with open(skip_marker_path, "w") as fout:
+                    fout.write(str(e))
             else:
                 profile = pd.DataFrame(
                     [{k: profile[k] for k in ticker_keys if k in profile}]
                 )
                 history_new.to_parquet(
-                    os.path.join(base_path, f"{region}-{symbol}-{dt}.parquet")
+                    os.path.join(
+                        historical_base_path, f"{region}-{symbol}-{dt}.parquet"
+                    )
                 )
 
                 # By placing the new dataframe prior to the existing one, we can easily re-order columns
@@ -188,4 +201,7 @@ def refresh_tickers_and_historical_data(
                 # This is wasteful, but an acceptable practice not to lose data
                 tickers.to_parquet(tickers_target_path)
 
-            time.sleep(random.random() * delay_factor)
+                time.sleep(random.random() * delay_factor)
+
+        historical = pd.read_parquet(historical_base_path)
+        historical.to_parquet(historical_target_path)
