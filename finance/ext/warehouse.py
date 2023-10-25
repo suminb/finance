aa09c2f1dd37330056b5e8a317793a7ae80a70e5
Sumin Byeon <suminb@gmail.com>
2023-10-25 22:33:06 +0900

    feat: Store historical data in staging files

--- finance/ext/warehouse.py
+++ finance/ext/warehouse.py
@@ -159,40 +159,30 @@ def refresh_tickers_and_historical_data(
         "updated_at",
     ]
 
-    history = historical_source.copy()
     with Progress() as progress:
         task = progress.add_task("[red]Fetching", total=len(symbols))
         for symbol in symbols:
             progress.update(task, description=f"Fetching[{symbol}]", advance=1)
+            dt = datetime.utcnow().strftime("%Y%m%d")
 
             try:
                 profile, history_new = fetch_profile_and_historical_data(symbol, region)
             except Exception as e:
                 log.warn(f"{symbol}: {e}")
-                profile = pd.DataFrame(
-                    [
-                        {
-                            "region": region,
-                            "symbol": symbol,
-                            "status": "error",
-                            "updated_at": datetime.utcnow(),
-                        }
-                    ]
-                )
-                history_new = None
             else:
                 profile = pd.DataFrame(
                     [{k: profile[k] for k in ticker_keys if k in profile}]
                 )
+                history_new.to_parquet(
+                    os.path.join(base_path, f"{region}-{symbol}-{dt}.parquet")
+                )
 
-            # By placing the new dataframe prior to the existing one, we can easily re-order columns
-            tickers = concat_dataframes(
-                profile, tickers, drop_duplicates_subset=["region", "symbol"]
-            )
-            history = concat_dataframes(history_new, history)
+                # By placing the new dataframe prior to the existing one, we can easily re-order columns
+                tickers = concat_dataframes(
+                    profile, tickers, drop_duplicates_subset=["region", "symbol"]
+                )
 
-            # This is wasteful, but an acceptable practice not to lose data
-            tickers.to_parquet(tickers_target_path)
+                # This is wasteful, but an acceptable practice not to lose data
+                tickers.to_parquet(tickers_target_path)
 
-            history.to_parquet(historical_target_path)
             time.sleep(random.random() * delay_factor)
